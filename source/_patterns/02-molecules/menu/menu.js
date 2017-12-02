2df82ccc8a1c115f4c3738a95abd6ec449958894
Valentin Palkovic <v.palkovic@getsafe.de>
2017-12-03 00:24:43 +0100

    Add letter and foundation section

--- source/_patterns/02-molecules/menu/menu.js
+++ source/_patterns/02-molecules/menu/menu.js
@@ -1,11 +1,28 @@
 import $ from 'jquery'
 
 const $icons = $('.menu svg').not(':first-child')
+const $views = $('.view > div')
 
 $icons.click((e) => {
-  const element = e.currentTarget
+  const $element = $(e.currentTarget)
   $icons.removeClass('menu__icon--active')
-  $(element).addClass('menu__icon--active')
+  $views.removeClass('view--active')
+  $element.addClass('menu__icon--active')
+
+  switch ($icons.index($element)) {
+    case 0:
+      $views.eq(0).addClass('view--active')
+      break
+    case 1:
+      $views.eq(1).addClass('view--active')
+      break
+    case 2:
+      $views.eq(2).addClass('view--active')
+      break
+    default:
+      break
+  }
 })
 
 $icons.eq(0).addClass('menu__icon--active')
+$views.eq(0).addClass('view--active')
