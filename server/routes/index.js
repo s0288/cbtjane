57e0689474b0686435a743b8c6098e547c7c708d
Valentin Palkovic <v.palkovic@getsafe.de>
2017-12-02 20:02:36 +0100

    Update npm

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,3 @@
-const json = require('../../source/_patterns/04-templates/01-chatbot.json')
-
 module.exports = (app) => {
   app.use('/huhu', (req, res) => {
     res.status(200)
--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,1 @@
-import $ from 'jquery'
-
 import 'core-js'
